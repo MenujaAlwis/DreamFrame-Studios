efadd0c7cef3ba93e355294cd75f41f8756cdb66
MenujaAlwis <menujaalwis@gmail.com>
2026-06-25 22:46:47 +0530

    add frontend constants file for services

---
+++ frontend/src/constants/services.js
@@ -0,0 +1,33 @@
+import weddingCoverImage from '../assets/services-wedding-cover-image.jpg';
+import preshootCoverImage from '../assets/services-preshoot-cover-image.jpg';
+import portraitCoverImage from '../assets/services-portrait-cover-image.jpg';
+import eventCoverImage from '../assets/services-event-cover-image.jpg';
+import commercialCoverImage from '../assets/services-commercial-cover-image.jpg';
+
+export const SERVICES = [
+  {
+    title: 'Wedding Photography',
+    category: 'wedding',
+    image: weddingImage
+  },
+  {
+    title: 'Pre-shoot Photography',
+    category: 'pre-shoot',
+    image: '/assets/preshoot.jpg'
+  },
+  {
+    title: 'Portrait Photography',
+    category: 'portrait',
+    image: '/assets/portrait.jpg'
+  },
+  {
+    title: 'Event Photography',
+    category: 'event',
+    image: '/assets/event.jpg'
+  },
+  {
+    title: 'Commercial Photography',
+    category: 'commercial',
+    image: '/assets/commercial.jpg'
+  }
+];
