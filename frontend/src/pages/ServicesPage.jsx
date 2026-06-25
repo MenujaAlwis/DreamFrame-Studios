db48c4c9d00b1aac74f237f33a9b8f255451ac05
MenujaAlwis <menujaalwis@gmail.com>
2026-06-25 22:50:57 +0530

    add services page

---
+++ frontend/src/pages/ServicesPage.jsx
@@ -0,0 +1,16 @@
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import ServicesGrid from '../components/ServicesGrid';
+import './ServicesPage.css';
+
+const ServicesPage = () => {
+  return (
+    <>
+      <Header />
+      <ServicesGrid />
+      <Footer />
+    </>
+  );
+};
+
+export default ServicesPage;
