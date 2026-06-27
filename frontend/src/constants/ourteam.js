c2176e2ee1245e8c0370dfb459021758e666de25
MenujaAlwis <menujaalwis@gmail.com>
2026-06-27 22:24:34 +0530

    add our team constants

---
+++ frontend/src/constants/ourteam.js
@@ -0,0 +1,35 @@
+import teammember1 from '../assets/teammember1.png';
+import teammember2 from '../assets/teammember2.png';
+import teammember3 from '../assets/teammember3.png';
+import teammember4 from '../assets/teammember4.png';
+import teammember5 from '../assets/teammember5.png';
+
+export const OUR_TEAM = [
+  {
+    name: 'Daniel Silva',
+    role: 'Photographer',
+    image: teammember1
+  },
+  {
+    name: 'Emma Clarke',
+    role: 'Editor',
+    image: teammember2
+  },
+  {
+    name: 'Mike Johnson',
+    role: 'Photographer',
+    image: teammember3
+  },
+  {
+    name: 'Sarah Williams',
+    role: 'Photographer',
+    image: teammember4
+  },
+  {
+    name: 'David Brown',
+    role: 'Editor',
+    image: teammember5
+  }
+];
+
+ 
