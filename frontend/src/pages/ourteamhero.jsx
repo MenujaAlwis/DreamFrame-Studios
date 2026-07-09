c594521d1ef724aa5ceb08ebe6ba27a10b0727df
MenujaAlwis <menujaalwis@gmail.com>
2026-07-09 21:53:37 +0530

    add OurTeamHero component; implement team member hotspots with hover info; update background image

--- frontend/src/pages/ourteamhero.jsx
+++ frontend/src/pages/ourteamhero.jsx
@@ -1,5 +1,64 @@
 import './ourteamhero.css';
-import bg from '../assets/ourteamhero.png';
+import bg from '../assets/ourteamhero1.png';
+
+const team = [
+  {
+    name: "Emma Williams",
+    role: "Lead Photographer",
+    position: {
+      top: "35%",
+      left: "17%"
+    }
+  },
+  {
+    name: "Sarah Johnson",
+    role: "Assistant Photographer",
+    position: {
+      top: "58%",
+      left: "32%"
+    }
+  },
+  {
+    name: "David Brown",
+    role: "Studio Manager",
+    position: {
+      top: "40%",
+      left: "42%"
+    }
+  },
+  {
+    name: "Sophia Miller",
+    role: "Creative Director",
+    position: {
+      top: "42%",
+      left: "52%"
+    }
+  },
+  {
+    name: "Mia Patinson",
+    role: "Sales Associate",
+    position: {
+      top: "35%",
+      left: "68%"
+    }
+  },
+  {
+    name: "James Wilson",
+    role: "Videographer",
+    position: {
+      top: "58%",
+      left: "80%"
+    }
+  },
+  {
+    name: "Olivia Smith",
+    role: "Photo Editor",
+    position: {
+      top: "40%",
+      left: "85%"
+    }
+  }
+];
 
 const OurTeamHero = () => {
   return (
@@ -7,12 +66,35 @@ const OurTeamHero = () => {
       className="ourteam-hero"
       style={{ backgroundImage: `url(${bg})` }}
     >
+
       <div className="ourteam-hero-content">
-        <h1 className="ourteam-title">Our Team</h1>
-        <p className="ourteam-subtitle">
+        <h1 className="ourteam-title">
           Behind the lens
-        </p>
+        </h1>
       </div>
+
+
+      {
+        team.map((person,index)=>(
+          <div
+            key={index}
+            className="person-hotspot"
+            style={{
+              top: person.position.top,
+              left: person.position.left
+            }}
+          >
+
+            <div className="person-info">
+              <h2>{person.name}</h2>
+              <p>{person.role}</p>
+            </div>
+
+          </div>
+        ))
+      }
+
+
     </div>
   );
 };
